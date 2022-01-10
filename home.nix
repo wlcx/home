6025f0130a38ec1896f9d4d7f6dd28de380e867a
Sam Willcocks <sam@wlcx.cc>
2022-01-10 15:58:07 +0000

    Autoformat

--- home.nix
+++ home.nix
@@ -3,9 +3,7 @@
 let mypkgs = (import ./modules/pkgs.nix { inherit pkgs; });
 in {
   home.packages = mypkgs.all;
-  home.sessionVariables = {
-    "EDITOR" = "nvim";
-  };
+  home.sessionVariables = { "EDITOR" = "nvim"; };
   programs = {
     home-manager.enable = true;
 
@@ -31,12 +29,13 @@ in {
       enable = true;
       settings = {
         add_newline = false;
-        format = "$username$hostname$shlvl$directory$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$docker_context$golang$kotlin$nodejs$python$rust$terraform$nix_shell$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$status$shell$character";
-        username.format = ''[$user]($style) '';
-        hostname.format = ''[$hostname]($style) '';
-        git_branch.format = ''[$symbol$branch]($style) '';
-        python.format = '' [py ''${pyenv_prefix}(''${version} )(\($virtualenv\) )]($style)'';
-        nodejs.format = '' [js ($version )]($style)'';
+        format =
+          "$username$hostname$shlvl$directory$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$docker_context$golang$kotlin$nodejs$python$rust$terraform$nix_shell$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$status$shell$character";
+        username.format = "[$user]($style) ";
+        hostname.format = "[$hostname]($style) ";
+        git_branch.format = "[$symbol$branch]($style) ";
+        python.format = "[py \${pyenv_prefix}(\${version} )(\\($virtualenv\\) )]($style)";
+        nodejs.format = "[js ($version )]($style)";
       };
     };
 

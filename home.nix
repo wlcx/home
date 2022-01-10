8d1aa84af3d7a8518ece0a60df63f568464bc3d0
Sam Willcocks <sam@wlcx.cc>
2022-01-10 17:01:29 +0000

    [home] Add exa, remove prompt dir truncation

--- home.nix
+++ home.nix
@@ -25,6 +25,11 @@ in {
       }];
     };
 
+    exa = {
+      enable = true;
+      enableAliases = true;
+    };
+
     starship = {
       enable = true;
       settings = {
@@ -33,6 +38,9 @@ in {
           "$username$hostname$shlvl$directory$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$docker_context$golang$kotlin$nodejs$python$rust$terraform$nix_shell$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$status$shell$character";
         username.format = "[$user]($style) ";
         hostname.format = "[$hostname]($style) ";
+        directory = {
+          truncation_length = -1;
+        };
         git_branch.format = "[$symbol$branch]($style) ";
         python.format = "[py \${pyenv_prefix}(\${version} )(\\($virtualenv\\) )]($style)";
         nodejs.format = "[js ($version )]($style)";

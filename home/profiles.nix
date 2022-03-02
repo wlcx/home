8d1dea1ccf5aa5044cd6212cef57269ad02d9edb
Sam Willcocks <sam@wlcx.cc>
2022-03-02 20:02:27 +0000

    Separate laptops profile, add password-store

--- home/profiles.nix
+++ home/profiles.nix
@@ -1,5 +1,6 @@
 {
-  dev = { ... }: {
-    imports = [ ./default.nix ./git.nix ./macs.nix ./rust.nix ./vim.nix ];
+  dev = { ... }: { imports = [ ./default.nix ./git.nix ./rust.nix ./vim.nix ]; };
+  laptop = { ... }: {
+    imports = [ ./default.nix ./git.nix ./macs.nix ./rust.nix ./vim.nix ./passwords.nix ];
   };
 }

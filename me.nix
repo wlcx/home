297c91a062b5fad9d643a99b813cc859f043f6fb
Sam Willcocks <sam@wlcx.cc>
2022-04-16 16:51:05 +0100

    me: move username and ssh keys to dedicated file

---
+++ me.nix
@@ -0,0 +1,11 @@
+{
+  username = "samw";
+  sshPubkeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHOKaZkJhTPLhD7bxO8bFxjXv2FEZCdYi35VEu9F0gEp samw@potassium-2.local"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIMvrgrLnE+AQBOBRiBoTFkbWaYTvqaBzIp4OfDiXUij"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOWevN05NeAAtKmTLlkZDHH7zlC90RtxDDgtcWh9zz6Wj2FaEA8ezpNjU400IaEfVR7pkhMKFOfMRRRozuFL+8Y= newcmrmac"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDQdGzwYiallvWXIHgSAf2GOwMJKA8bxPmwyuO+vsd1HwB65hMRPCpKS+FNLIpkrADNnuhGS3xGCGSSuQ+zAu/g= zinc-se-main@secretive.zinc.local"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNLrJyUXmiFWb9vhlTWZLYr64IsKut+c9TGqq3/uwPDeF4X0Qb2jzxqXfQcDSztjR09JHbC8BOqfpYYT9LHahIo= YubiKey #13340583 PIV Slot 9a"
+  ];
+}
+

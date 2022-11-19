d2d59eefa04ee0c28c55047683b908cbfcee9819
Sam Willcocks <sam@wlcx.cc>
2022-11-19 22:35:30 +0000

    vim: Tweak textwidth, fix python autoindent

--- home/init.lua
+++ home/init.lua
@@ -2,12 +2,13 @@
 vim.o.number = true
 vim.o.relativenumber = true  -- number + relativenumber == hybrid
 vim.o.mouse = "nvi"  -- mouse mode in normal, visual and insert
-vim.o.textwidth = 88  -- A vaguely sensible default textwidth
-vim.o.colorcolumn = "+0"  -- Mark the textwidth
+vim.o.textwidth = 88  -- a vaguely sensible default textwidth
+vim.o.formatoptions = "cqj"  -- the default, but minus `t` which autowraps text.
+vim.o.colorcolumn = "+0"  -- mark the textwidth
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
-vim.o.expandtab = true
+vim.o.expandtab = true  -- owo expands ur tab
 
 vim.o.list = true
 vim.o.listchars = "trail:·"  -- show trailing spaces
@@ -18,4 +19,7 @@ vim.o.termguicolors = true
 
 -- Keybinds
 vim.api.nvim_set_keymap('n','<C-P>', '<cmd> FZF<CR>', { noremap=true })
+
+-- Language-specific
+vim.g.pyindent_open_paren = "shiftwidth()"  -- Don't double indent after ( you villain.
 

d90384b2de549876a26dfac31aeb7cd49fdc598f
Sam Willcocks <sam@wlcx.cc>
2025-12-05 15:55:32 +0000

    starship: add direnv

--- home/default.nix
+++ home/default.nix
@@ -76,16 +76,19 @@ in
       enable = true;
       settings = {
         add_newline = false;
-        format = "$username$hostname$shlvl$directory$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$docker_context$golang$kotlin$nodejs$python$rust$terraform$nix_shell$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$status$shell$character";
+        direnv.disabled = false;
+        format = "$all";
         username.format = "[$user]($style) ";
         hostname.format = "[$hostname]($style) ";
         directory = {
           truncation_length = -1;
         };
         git_branch.format = "[$symbol$branch]($style) ";
-        python.format = "[py \${pyenv_prefix}(\${version} )(\\($virtualenv\\) )]($style)";
-        nodejs.format = "[js ($version )]($style)";
-        nix_shell.format = "[nix $state( \\($name\\))]($style) ";
+        python.symbol = "py ";
+        nodejs.symbol = "js ";
+        nix_shell.symbol = "nix ";
+        rust.symbol = "rs ";
+        direnv.symbol = "de ";
       };
     };
 

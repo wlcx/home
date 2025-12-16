ac362b66672c5f4f1ac384c25404c6765972ce96
Sam Willcocks <sam@wlcx.cc>
2025-12-16 14:41:02 +0000

    macs: squish home manager warning (again?)

--- home/macs.nix
+++ home/macs.nix
@@ -17,5 +17,5 @@
       "~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
   };
   programs.zsh.sessionVariables.SSH_AUTH_SOCK = "$HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
-  programs.zsh.initExtra = "eval $(/opt/homebrew/bin/brew shellenv)";
+  programs.zsh.initContent = "eval $(/opt/homebrew/bin/brew shellenv)";
 }
